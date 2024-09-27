beb0f70dde5798fcad08f5d26b7b77a0733c05ea
harishrishee <34533755+harishrishee@users.noreply.github.com>
2024-09-27 17:02:03 +0000

    updated details

--- src/containers/skills/CypressImg.js
+++ src/containers/skills/CypressImg.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 
 export default class CypressImg extends Component {
   render() {
-    //const theme = this.props.theme;
     return (
       <img
-        className="cypress"
-        src={require("../../assests/images/product-iteration-animate.svg")}
+        className="performance"
+        src={`${process.env.PUBLIC_URL}/icons/product-iteration-animate.svg`}
         alt="Logo"
       />
     );
--- src/containers/skills/DesignImg.js
+++ src/containers/skills/DesignImg.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 
 export default class DesignImg extends Component {
   render() {
-    //const theme = this.props.theme;
     return (
       <img
-        className="raw_logo"
-        src="/icons/performance-overview-animate.svg"
+        className="performance"
+        src={`${process.env.PUBLIC_URL}/icons/performance-overview-animate.svg`}
         alt="Logo"
       />
     );
