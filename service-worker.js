b3784d4738a4be9dadc93ccba64cd90fb29859fb
harishrishee <harishrishee@users.noreply.github.com>
2024-09-27 16:43:54 +0000

    deploy: 722324ad87a9825adc60f444f16c2554931f3184

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a240cc32195dd05aeff0cd5925150d9a.js"
+  "/myportfolio/precache-manifest.dc6b889f112c1217e69a5ae1782d88dc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
